dbecf9f34bc71f826fc61a879e6208b67357c679
alexandrekarst <alexandre.karst@iadvize.com>
2017-10-11 09:50:37 +0200

    Project with framework generation

---
+++ Archives/MySDK 2017-10-11 09-15-50/Products/Library/Frameworks/MySDK.framework/Headers/MySDK.h
@@ -0,0 +1,19 @@
+//
+//  MySDK.h
+//  MySDK
+//
+//  Created by AlexandreKARST on 10/10/2017.
+//  Copyright © 2017 iAdvize. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySDK.
+FOUNDATION_EXPORT double MySDKVersionNumber;
+
+//! Project version string for MySDK.
+FOUNDATION_EXPORT const unsigned char MySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDK/PublicHeader.h>
+
+
---
+++ MySDK/MySDK.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  MySDK
+//
+//  Created by AlexandreKARST on 10/10/2017.
+//  Copyright © 2017 iAdvize. All rights reserved.
+//
+
+import Foundation
+
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
